845b7cfbaf87770b5dc80e5019757dbd571bc316
A-Francis <frankyfray2000@gmail.com>
2023-08-29 20:45:18 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,8 +3,8 @@
 /**
  * delete_nodeint_at_index - deletes a node in a linked list at a certain index
  * @head: pointer to the first element in the list
- * @index: index of the node to delete
- * Return: 1 (Success), or -1 (Fail)
+ * @index: index of the node to be deleted
+ * Return: 1 to show (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
