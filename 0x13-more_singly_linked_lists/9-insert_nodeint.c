17874a40bdb312b2629583791335360374e60f15
A-Francis <frankyfray2000@gmail.com>
2023-08-29 20:40:11 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,32 +1,31 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
+ * insert_nodeint_at_index - inserts a node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
+ * @idx: index where the  node is added
+ * @n: data to insert in the  node
  * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *nw;
+	listint_t *new;
 	listint_t *temp = *head;
 
-	nw = malloc(sizeof(listint_t));
-	if (!nw || !head)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	nw->n = n;
-	nw->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
 		new->next = *head;
-		*head = nw;
-		return (nw);
+		*head = new;
+		return (new);
 	}
 
 	for (i = 0; temp && i < idx; i++)
@@ -34,8 +33,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new->next = temp->next;
-			temp->next = nw;
-			return (nw);
+			temp->next = new;
+			return (new);
 		}
 		else
 			temp = temp->next;
